72bd1103b545e3ab6655a8c1e5f71f209882cd0e
asad-developer <muhammadasad-developer@gmail.com>
2025-10-14 22:36:16 -0700

    Initial commit with only index.html, style.css, and app.js

---
+++ app.js
@@ -0,0 +1,186 @@
+// Question 1:
+
+
+// Write a script to greet your website visitor using a JS alert box.
+
+
+// Answer:
+
+// alert("Welcome to my websites!")
+
+
+
+
+
+
+
+// Question 2:
+
+
+// Write a script to display a message on your web page 
+
+
+// Answer:
+
+// document.write("Hello bro");
+
+
+// Question 3:
+
+// Write a script to display a message with a line break(use \n for new line).
+
+
+// Answer:
+
+// alert("Welcome to college land\nHappy birthday!");
+
+// Question 4:
+
+
+//  Write a script to display two alert message in sequence (one after another).
+
+
+
+// Answer:
+
+// alert("Welcome to my websites!")
+
+// alert("Welcome to my college!")
+
+
+
+
+
+// Question 5:
+
+// Write a script to generate a message in the browsers console(use console.log()).
+
+
+
+// Answer:
+
+
+//  let first=("Asad")
+
+//  let last=("Ahmadani Balouch")
+
+//  console.log(first+" "+last)
+
+
+
+
+// let num5=(5)
+
+// let num3=(9)
+
+// console.log(num5+num3)
+
+
+
+
+
+
+// Question 6:
+
+// make use of alerts in your existing html & css project. 
+
+
+
+
+// Answer:
+
+// alert("Welcome to my websites!")
+
+
+
+// Question 7:
+
+// practice placing the <script> element in different parts of your project:
+// a) in the <head> section 
+// b) in the <body> before your html content 
+// c) in the <body> inside your html content 
+// d) in the <body> after your html content 
+
+
+
+// Answer:
+
+// a section 
+
+
+/* <head>
+
+    <script>
+
+        alert("Welcome to my websites!")
+
+
+    </script>
+
+
+
+
+</head> */
+
+
+// b section 
+
+/* <body>
+
+    <script>
+
+        alert("Welcome to my websites!")
+
+
+    </script>
+
+
+
+
+</body>  */
+
+
+// c section 
+
+
+/* <body>
+<h1>welcom to my web</h1>
+    <script>
+
+        alert("Welcome to my websites!")
+
+
+    </script>
+
+<p>welcom to my web .....................</p>
+
+
+</body> 
+
+
+
+
+
+
+
+
+
+// d section 
+
+
+<body>
+    <h1>welcom to my web</h1>
+    <p>welcom to my web .....................</p>
+
+    <script>
+
+        alert("Welcome to my websites!")
+
+
+    </script>
+
+
+
+</body>
+
+
